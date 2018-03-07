4e8cb18291c48625b7cb6b51b1aa2e3e2ea6fe89
Vincent Kwok <vkwok22@gmail.com>
2018-03-06 20:47:19 -0600

    created app for terminal

--- frontend.rb
+++ frontend.rb
@@ -6,13 +6,35 @@ response = Unirest.get("http://localhost:3000/all_products")
 products = response.body
 puts JSON.pretty_generate(products)
 
-# puts "(1) Show all products"
-# puts "(2) Show one product"
-# choice = gets.chomp
-# if choice == "1"
-#   response = Unirest.get("http://localhost:3000/all_products")
-#   products = response.body
-#   puts JSON.pretty_generate(products)
-# elsif choice == "2"
-#   puts "Which product would you like to see?"
-# end
+
+
+# table = TTY::Table.new ['Name', 'Price', 'Link', 'Description']
+#   ['header1','header2','header3','header4'], [
+#     ['a1', 'a2', 'a3', 'a4'], 
+#     ['b1', 'b2', 'a3', 'a4'],
+#     ['c1', 'c2', 'a3', 'a4'], 
+#     ['d1', 'd2', 'a3', 'a4'],
+#     ['e1', 'e2', 'a3', 'a4'], 
+#     ['f1', 'f2', 'a3', 'a4'],
+#     ['g1', 'g2', 'a3', 'a4'],
+#   ]
+# table.render(:ascii)
+# =>
+#  +-------+-------+-------+-------+
+#  |header1|header2|header3|header4|
+#  +-------+-------+-------+-------+
+#  |a1     |a2     |a3     |a4     |
+#  +-------+-------+-------+-------+
+#  |b1     |b2     |b3     |b4     |
+#  +-------+-------+-------+-------+
+#  |c1     |c2     |c3     |c4     |
+#  +-------+-------+-------+-------+
+#  |d1     |d2     |d3     |d4     |
+#  +-------+-------+-------+-------+
+#  |e1     |e2     |e3     |e4     |
+#  +-------+-------+-------+-------+
+#  |f1     |f2     |f3     |f4     |
+#  +-------+-------+-------+-------+
+#  |g1     |g2     |g3     |g4     |
+#  +-------+-------+-------+-------+
+
