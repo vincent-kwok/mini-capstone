02011d9b19c2d78df182a2f30699afd55fe1e1d0
Vincent Kwok <vkwok22@gmail.com>
2018-03-06 19:59:54 -0600

    individual pages added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,23 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def oven
+    product = Product.find_by(name: "oven")
+    render json: product.as_json
+  end
+  def microwave
+    product = Product.find_by(name: "microwave")
+    render json: product.as_json
+  end
+  def refridgerator
+    product = Product.find_by(name: "refridgerator")
+    render json: product.as_json
+  end
+  def toaster
+    product = Product.find_by(name: "toaster")
+    render json: product.as_json
+  end
+
+  # disadvantage: you would have to create a page for every single product
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/all_products" => "products#all_products"
+  get "/oven" => "products#oven"
+  get "/microwave" => "products#microwave"
+  get "/refridgerator" => "products#refridgerator"
+  get "/toaster" => "products#toaster"
 end
