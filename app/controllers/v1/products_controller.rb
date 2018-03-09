d89cd6cd32adc10458ef410084740af0ed8751a8
Vincent Kwok <vkwok22@gmail.com>
2018-03-08 18:34:54 -0600

    make restful

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,25 +1,28 @@
 class V1::ProductsController < ApplicationController
-  def all_products
+  def index
     products = Product.all
     render json: products.as_json
   end
 
-  def oven
-    product = Product.find_by(name: "oven")
-    render json: product.as_json
-  end
-  def microwave
-    product = Product.find_by(name: "microwave")
-    render json: product.as_json
-  end
-  def refridgerator
-    product = Product.find_by(name: "refridgerator")
-    render json: product.as_json
-  end
-  def toaster
-    product = Product.find_by(name: "toaster")
-    render json: product.as_json
+  def show
+    # text = Product.find_by(id: params["id"])
+    render json: Product.find_by(id: params["id"]).as_json
   end
 
-  # disadvantage: you would have to create a page for every single product
+  # def oven
+  #   product = Product.find_by(name: "oven")
+  #   render json: product.as_json
+  # end
+  # def microwave
+  #   product = Product.find_by(name: "microwave")
+  #   render json: product.as_json
+  # end
+  # def refridgerator
+  #   product = Product.find_by(name: "refridgerator")
+  #   render json: product.as_json
+  # end
+  # def toaster
+  #   product = Product.find_by(name: "toaster")
+  #   render json: product.as_json
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
-    get "/all_products" => "products#all_products"
-    get "/oven" => "products#oven"
-    get "/microwave" => "products#microwave"
-    get "/refridgerator" => "products#refridgerator"
-    get "/toaster" => "products#toaster"
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
   end
 end
